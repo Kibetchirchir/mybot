a94aaa666014fb7aaeeb4f49081300b12ff769ec
kelvin chirchir <langatchirchir@gmail.com>
2019-09-20 15:05:19 +0300

    initiate develop branch

--- app.js
+++ app.js
@@ -26,18 +26,16 @@ ROUTES
  */
 
 app.get('/' , function (req, res){
-
-res.send ("HI welcome");
+    if(req.query['hub.verify_token']==="chirchir"){
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('wrong token');
 });
 /**
  * token
  */
 
-//let token = 'EAADUxvcXkUUBAMHJKRihq21puCwj6HO5WaJ1ZAXZAVyMdN5VYZAgjGZAtTQ3BSbcki8InVZBGiPOjrrMvZCucKD3zE6DQVtmzZAafQzcOZCsg6c7xMy9TmYBEiZCt1L40Y52D7dbdB0Hf5kDbZAvdWvZAleiJPI5piyg7kFMAZAkZBj9U2WvkEkTPD1yj';
-// nouveta let token = 'EAADUxvcXkUUBABgmyHgbiwZBcGPykKz0ZB0jurLrV8JkJcNPWJ6ZAGIZCAKNaFR65r34wttH7XC5lNqB8ZCtqZAv3bvQd4rzVmhzVXjaoqgaDHQcGTBOcZAzsVxuFuuwZCmSzQ54XZBPConKIASPnx8lldSyEjfQlB1YSW7JnR8SXYvSPOlw4K6ui'
-//let token = "EAADUxvcXkUUBAKynDiAdz4l47j9uJupmbqXQ05ZAfCexmYtAoZC3W1YYuJMnxgryLcvu3tEaIZBYeZAU0hYmGV8lEGeeiXRGvSPvKQ0RxOMGZBQJZBtKiZCpFpjbtiVerJFQVYivtGfTuek3h5cSEEpCif3ZBLmfItw6OEe6x12RpPNoiomlMZAmO"
-//let token = "EAADUxvcXkUUBAE213i5BGeQTPXmFEJQJZAoqZCliz66n1b8ctZAiL2tfBZAKkYTvrLRbQtzBTKaT5A0iZCxgA7mu8Wo59NrXFLFndKZBvpOifzOtyXbckJ7ZAwcSZCT1NsEVzdMwH8rSoSuODzwjN1wt09F3k6xmYUAc18vlebz4dlvKeZBeW6ZCuf"
-let token = "EAADUxvcXkUUBAIs1XyodXQ1lbRRZB9KLhypCirHIADxXZB0DFQZAvkHhjMSLl5KnVrIOJWVbUIawC28jzlH4KozPKSifoyMZAkWq9wXVkR3suumZBTh2ZAPeqEDc6yQLQaf58pJ2o6uZCbZBPW9tZAkTLT8j2k2no3w1DPRN26IvPIGQDZAFliGMBq"
+let token = "EAAGzfOcz3wUBAMddqEbjkWoA4b1wcTQ2a9T5awqHASrATnQcqcTJsH2MO3WYu0XfvdyUJ7HkSJtMKKPDgDP4XBY3MZAq0B5pGR1VaFLm85WnVTCQsRIyy1UUVup5YvIwf4g6kaA0WIuU6VVHTcef0R8ymZC4WXn5EEOC6l3gZDZD"
 /**
  * FACEBOOK
  */
